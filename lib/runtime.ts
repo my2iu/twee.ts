c301cca7272135d0f63e43c8f726dd9425a14247
Ming <github@programmingbasics.org>
2015-05-06 20:11:29 -0400

    Abstracting out passage output a bit more to make things easier later, and added supporting for including and falling through to other passages.

--- lib/runtime.ts
+++ lib/runtime.ts
@@ -6,7 +6,7 @@ declare var marked : any;
 class Passage {
     constructor(public name : string, public codename: string, public tags: string[]) {
     }
-	run() : string { return ''; }
+	run(_$_: PassageOutput) : void { }
 	hasTag(name: string) : boolean { 
 		for (let n = 0; n < this.tags.length; n++)
 			if (this.tags[n] == name) 
@@ -43,12 +43,22 @@ class Passage {
 
 var passages : Passage[] = [];
 
-function htmlEscape(str : string) : string {
-	return $("<div>").text(str).html();
+function htmlEscape(str : any) : string {
+	return $("<div>").text('' + str).html();
 }
 
 const TWINETS_PASSAGE_SCHEMA = 'twine.ts+passage:';
 const TWINETS_FUNCTION_SCHEMA = 'twine.ts+function:';
+
+class PassageOutput {
+	output : string = '';
+	out(str: any) {
+		this.output += '' + str;
+	}
+	mergeIn(merge: PassageOutput) {
+		this.output += merge.output;
+	}
+}
 
 class Story {
 	startPassageName: string = 'Start';
@@ -56,6 +66,22 @@ class Story {
 	visitedPassages: { [key:string]:boolean } = {};
 	currentPassage: Passage = null;
 	previousPassage : Passage = null;
+
+	/**
+	 * When we started this "turn" with the user clicking on something, what was
+	 * the currentPassage? (Later on, we might have fallen through to another passage,
+	 * which changed the currentPassage to something else.)
+	 */
+	firstCurrentPassage: Passage = null;
+	previousFirstCurrentPassage: Passage = null;
+	
+	/**
+	 * As passages include and fallthrough to other passages, their output 
+	 * objects are stored in a stack. This allows a passage to call an
+	 * outside function, and that function can then add to the output of the
+	 * current passage without needing an explicit copy of the output object.
+	 */
+	passageOutputStack : PassageOutput[] = [];
 	
 	init() {
 		// Make a proper map of all the passages
@@ -97,13 +123,33 @@ class Story {
 		return name;
 	}
 	
-	show(passage : Passage) : void {
-		this.previousPassage = this.currentPassage;
-		this.currentPassage = passage;
+	currentPassageOutput() : PassageOutput {
+		return this.passageOutputStack[this.passageOutputStack.length - 1];
+	}
+	
+	runPassage(passage: Passage) : PassageOutput 
+	{
 		this.visitedPassages[passage.name] = true;
+		
+		let output = new PassageOutput();
+		this.passageOutputStack.push(output);
+		try {
+			passage.run(output);
+		} finally {
+			this.passageOutputStack.pop();
+		}
+		return output;
+	}
+	
+	show(passage : Passage) : void {
+		this.previousFirstCurrentPassage = this.firstCurrentPassage;
+		this.previousPassage = this.currentPassage;
+		this.firstCurrentPassage = passage;
+		this.currentPassage = passage;
 	
 		// Run the passage code to get the Markdown to show
-		var text = passage.run();
+		let output = this.runPassage(passage);
+		var text = output.output;
 		
 		// Render the Markdown and put it onto the web page
 		$("#passage").html(this.parseMarkdown(text, ''));
@@ -156,3 +202,15 @@ function visited(passage: Passage) : boolean
 	return story.visitedPassages[passage.name];
 }
 
+function include(passage: Passage) : void
+{
+	// Get the current output context
+	var output = story.runPassage(passage);
+	story.currentPassageOutput().mergeIn(output);
+}
+
+function fallthrough(passage: Passage) : void
+{
+	story.currentPassage = passage;
+	include(passage);
+}
